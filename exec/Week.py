1651e9e935ed0fd9ac35d59b09a009da9d7b6bc6
Phil Hunt <philiphuntiv2@gmail.com>
2025-11-25 15:29:58 -0500

    Update Week.py

--- exec/Week.py
+++ exec/Week.py
@@ -7,10 +7,13 @@ for gen in (mnt.E.descendants(), mnt.C.descendants()):
         s = p.seg()
 
         if s.startswith('.'):
+            print(p)
             p.visibility.hide()
             
         elif s.startswith('__') and s.endswith('__'):
+            print(p)
             p.visibility.hide()
 
         elif s in ['.DS_Store', 'Thumbs.db', 'desktop.ini']:            
+            print(p)
             p.visibility.hide()
