f7d0fc4610fc7acc51e51127da21f9e1746969ba
Phil Hunt <philiphuntiv2@gmail.com>
2025-11-26 10:51:39 -0500

    Update Day.py

--- exec/Day.py
+++ exec/Day.py
@@ -2,7 +2,7 @@ from __init__ import this
 from philh_myftp_biz.modules import Scanner
 
 # Backup Files
-this.run('run', 'backup', False)
+#this.run('run', 'backup', False)
 
 # Iter through all main modules
 for mod in Scanner():
