0b944c96cf060e865a403ac0d4bd3b85bf2e8bd3
Phil Hunt <philiphuntiv2@gmail.com>
2025-11-27 13:36:16 -0500

    Update Day.py

--- exec/Day.py
+++ exec/Day.py
@@ -1,11 +1,11 @@
-from __init__ import this
 from philh_myftp_biz.modules import Scanner
-
-# Backup Files
-#this.run('run', 'backup', False)
 
 # Iter through all main modules
 for mod in Scanner():
 
     # Install/Upgrade all dependencies
     mod.install()
+
+    mod.git('add', '.')
+
+    mod.git('commit', '-a', '-m', 'Automated Commit')
