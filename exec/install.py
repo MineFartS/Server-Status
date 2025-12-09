113fbb357ed2287d438046b0c379e35ae628486a
Phil Hunt <philiphuntiv2@gmail.com>
2025-12-09 09:03:47 -0500

    Update install.py

--- exec/install.py
+++ exec/install.py
@@ -1,16 +1,14 @@
-from importlib.util import find_spec
-from pip._internal import main
+from subprocess import run
+from sys import executable
 
-if find_spec('philh_myftp_biz') is None:
-    main(['install', 'philh_myftp_biz'])
+run(
+    args = [executable, '-m', 'pip', 'install', '.'],
+    cwd = 'C:/Users/Administrator/Documents/GitHub/philh_myftp_biz/'
+)
 
-from philh_myftp_biz.modules import Module
+from philh_myftp_biz.modules import Scanner
 from philh_myftp_biz.pc import Path
 
-for p in Path('E:/').children():
+for m in Scanner():
     
-    try:
-        Module(p).install(False)
-    
-    except ModuleNotFoundError:
-        pass
+    m.install(hide=False)
