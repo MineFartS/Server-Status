3ec63a4fe08f14db885dd401d1ca927c7d9dbfc8
Phil Hunt <philiphuntiv2@gmail.com>
2026-01-31 22:03:05 -0500

    Update Hour.py

--- Interval/Hour.py
+++ Interval/Hour.py
@@ -2,6 +2,7 @@ from philh_myftp_biz.modules import ServiceDisabledError
 from ..Items.Services import Services
 from philh_myftp_biz.time import now
 from philh_myftp_biz.pc import Path
+from philh_myftp_biz.terminal import Log
 
 # ==================================================
 
@@ -18,18 +19,28 @@ for service in Services:
 
 temp = Path('E:/__temp__')
 
-minAge = (now().unix - 3600*20) # 20 hours ago
+MINIMUM = (now().unix - 3600*20) # 20 hours ago
 
 # Get all files in the temp dir
 for p in temp.descendants():
-
-    #
     if p.isfile():
+
+        MODIFIED = p.mtime.get()
+        DIFF     = (MINIMUM - MODIFIED.unix)
+
+        DELETE = (DIFF > 0)
+
+        Log.INFO(
+            f'Scanning File\n'+ \
+            f'{str(p)=}\n'+ \
+            f'{MODIFIED.ISO=}\n' +\
+            f'{DIFF=}\n'+ \
+            f'{DELETE=}'
+        )
     
-        # If file is more than 20 hours old
-        if p.mtime.get() < minAge:
+        if DELETE:
 
             # Delete File
-            p.delete()
+            p.delete(force=True)
 
 # ==================================================
