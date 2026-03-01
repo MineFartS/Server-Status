5126fcbe2cfdccf06b653c866ae1b741819956c3
Phil Hunt <philiphuntiv2@gmail.com>
2026-03-01 10:21:46 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,17 +1,17 @@
-from philh_myftp_biz.web import online, get
+from philh_myftp_biz.web import is_online, get
 from philh_myftp_biz.modules import Module
 from philh_myftp_biz.terminal import Log
 from philh_myftp_biz.file import YAML
 
 this = Module('C:/Scripts')
 
-options = YAML(this.file('options')).read()
+options: dict = YAML(this.file('options')).read()
 
-def alert(m):
+def alert(m:str) -> None:
 
     Log.INFO(f'Alert: {m}')
 
-    if online() and options['alert']['sms']:
+    if is_online() and options['alert']['sms']:
         # Send SMS Alert
         get(
             url = 'https://script.google.com/macros/s/AKfycbztUPSGrf9lnl3_qIBjBui6pDSl_errjxdEi_9LnGNO0TjgMeZDBvWvrQBU36P80x0/exec',
