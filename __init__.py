670c971647d719523eea68124fb25e7ce9b6f512
Phil Hunt <philiphuntiv2@gmail.com>
2026-03-08 19:49:59 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,29 +1,30 @@
+# ===============================================================================================================
 
-try:
+# Run 'Setup.py' if philh_myftp_biz is not installed
+try:    
     import philh_myftp_biz # pyright: ignore[reportUnusedImport]
 except ModuleNotFoundError:
     from . import Setup # pyright: ignore[reportUnusedImport]
 
-from philh_myftp_biz.web import is_online, get
-from philh_myftp_biz.modules import Module
+# ===============================================================================================================
+
 from philh_myftp_biz.terminal import Log
-from philh_myftp_biz.file import YAML
+from philh_myftp_biz.web import get
+from .Items import Modules
 
-this = Module('C:/Scripts')
+# ===============================================================================================================
 
-options: dict = YAML(this.file('options')).read()
+def alert(msg:str) -> None:
 
-def alert(m:str) -> None:
+    Log.MAIN(msg)
 
-    Log.INFO(f'Alert: {m}')
+    # Send SMS Alert
+    get(
+        url = 'https://script.google.com/macros/s/AKfycbyKYV2-Q1JUtMr1FdKR5eP-Bxy5m1EOd8EOFH9ghueqN-zZqQpPa9PEi8ZFz5T5uQc1Xg/exec',
+        params = {'message': msg}
+    )
 
-    if is_online() and options['alert']['sms']:
-        # Send SMS Alert
-        get(
-            url = 'https://script.google.com/macros/s/AKfycbyKYV2-Q1JUtMr1FdKR5eP-Bxy5m1EOd8EOFH9ghueqN-zZqQpPa9PEi8ZFz5T5uQc1Xg/exec',
-            params = {'message' : m}
-        )
+    # Show Alert Box
+    Modules[0].start('vbs/alert', msg)
 
-    if options['alert']['popup']:
-        # Show Alert Box
-        this.start('exec/alert', m)
+#======================
