7f0594c4e95d7037b9051a5da7869b61d2ad477c
Phil Hunt <philiphuntiv2@gmail.com>
2026-03-09 15:59:30 -0400

    Create Update.py

---
+++ Utils/Update.py
@@ -0,0 +1,7 @@
+from subprocess import run
+from sys import executable
+
+run(
+    args = [executable, '-m', 'pip', 'install', '.'],
+    cwd = 'C:/Users/Administrator/Documents/GitHub/philh_myftp_biz/'
+)
